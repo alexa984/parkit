f1bfc2f4be0b432ee2865e8f4307c00a5c87aa59
alexa984 <alex.yovkova@gmail.com>
2020-04-23 02:19:19 +0300

    Show some logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css'
+import logo from '../../images/logo.png';
 
 class Header extends React.Component {
   render() {
     return (
       <div className={styles.navigationHeader}>
+        <img src={logo} alt="logo" className={styles.logo} />
         <Link to="/" className={styles.navTitle}> ParkIt </Link>
       </div>
     );
