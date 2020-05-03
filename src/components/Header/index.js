98870bfa3b1787293c041aef877a2d2ba0b881c8
Bozhidara Pachilova <bozhidara.p@abv.bg>
2020-05-04 00:20:58 +0300

    Added new resposive navbar and Map image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,30 @@
+
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import styles from './styles.module.css'
 import logo from '../../images/logo.png';
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import styles from './styles.module.css'
 
 class Header extends React.Component {
-  render() {
-    return (
-      <div className={styles.navigationHeader}>
-        <img src={logo} alt="logo" className={styles.logo} />
-        <Link to="/" className={styles.navTitle}> ParkIt </Link>
-      </div>
-    );
-  }
+render() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className={styles.navigationHeader}>
+        <Nav className="mr-auto">
+          <img src={logo} alt="logo" className={styles.logo} />
+          <Navbar.Brand href="/" className={styles.navTitle}>ParkIt</Navbar.Brand>
+      </Nav>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav" className={styles.navLinkGroup}>
+      <Nav className="ml-auto">
+        <Nav.Link href="/" className={styles.navLink}>Home</Nav.Link>
+        <Nav.Link href="/Map" className={styles.navLink}>Map</Nav.Link>
+        <Nav.Link href="/profile/index.js" className={styles.navLink}>Profile</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  );
+}
 }
 
 export default Header;
--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -3,17 +3,19 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 
 import { Header } from 'components';
+import map from '../../images/map.png';
+import styles from './styles.module.css';
 
 export default class Map extends React.Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <Container>
-          Map page.
-        </Container>
-      </>
-    );
-  }
+render() {
+return (
+<>
+  <Header />
+  <div className={styles.flexContainer}>
+    <img src={map} alt="map"/>
+  </div>
+</>
+);
+}
 }
 
