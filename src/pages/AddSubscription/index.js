2c1a679dbd2231289b9f9c53772da9a017334176
alexa984 <alex.yovkova@gmail.com>
2020-05-12 03:30:13 +0300

    Little style changes on subscriptions

--- src/pages/AddSubscription/index.js
+++ src/pages/AddSubscription/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Button, Icon } from 'semantic-ui-react';
+import { Button, Image, Grid } from 'semantic-ui-react';
+import { toast } from 'react-toastify';
 import * as cx from 'classnames';
 import { Header } from 'components';
 import styles from './styles.module.css';
-import Card from '../../images/Card.png';
-import Calendar from '../../images/icons/Calendar.svg';
+import Card from 'images/Card.png';
+import Calendar from 'images/icons/Calendar.svg';
+import ParkingSubscriptionIconWhite from 'images/icons/ParkingSubscriptionIconWhite.svg';
+import RemoteControl from 'images/icons/RemoteControlWhite.svg';
 
 
 export default class AddSubscription extends React.Component {
+  delay = ms => new Promise(res => setTimeout(res, ms));
+
+  showToast = async () => {
+    toast.info("Successfully added subscription.");
+    await this.delay(2000)
+  }
 
   render() {
     return (
@@ -21,14 +30,28 @@ export default class AddSubscription extends React.Component {
         </div>
 
         <div className={styles.selectSubscriptionType}>
-          <div className={styles.flexContainer}>
-            <button className={`${styles.addSubscriptionBtn} ${styles.parkingCard}`}>
-              <p>Parking Card</p>
-            </button>
-            <button className={`${styles.addSubscriptionBtn} ${styles.rc}`}>
-              <p>Remote Control</p>
-            </button>
-          </div>
+          <Grid columns={4} className={styles.center}>
+            <Grid.Row className={styles.pushDown}>
+              <Grid.Column key={10} width={3} />
+              <Grid.Column key={10} width={5}>
+                <Image src={ParkingSubscriptionIconWhite} alt="parking card" className={styles.center} />
+              </Grid.Column>
+              <Grid.Column key={2} width={5}>
+                <Image src={RemoteControl} alt="remote control" className={styles.center} />
+              </Grid.Column>
+              <Grid.Column key={10} width={3} />
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column key={10} width={3} />
+              <Grid.Column key={10} width={5}>
+                <p className={styles.cardTitle}>Parking Card</p>
+              </Grid.Column>
+              <Grid.Column key={2} width={5}>
+                <p className={styles.cardTitle}>Remote Control</p>
+              </Grid.Column>
+              <Grid.Column key={10} width={3} />
+            </Grid.Row>
+          </Grid>
         </div>
         <div className={styles.flexContainer}>
           <img src={Card} alt="Card" className={styles.cardPic} />
@@ -53,17 +76,16 @@ export default class AddSubscription extends React.Component {
             </div>
           </div>
           <div className={styles.flexContainerColumn}>
-            <Button className={styles.uploadButton} htmlFor="file" type="button" size="small" icon labelPosition='right'>
+            <Button as="label" htmlFor="file" type="button" className={styles.uploadButton}>
               Upload validation document
-              <Icon name='file' color="blue" />
             </Button>
-            <input type="file" id="file" hidden />
+            <input type="file" id="file" hidden onChange={this.onChange} />
           </div>
         </div>
 
 
-        <a className={styles.button} href="/profile#popup2">
-          <button className={styles.addSubscription}></button>
+        <a className={styles.button} href="/profile#popup3">
+          <button className={styles.addSubscription} onClick={this.showToast}></button>
         </a>
         {/* TODO? show success message when adding a subscription */}
 
