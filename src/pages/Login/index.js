22a0f082109e715f04d068728037b9156a5fe6c5
alexa984 <a.yovkova@hacksoft.io>
2020-05-29 20:38:41 +0300

    Add forgoten password and remember me buttons in login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,50 +1,92 @@
-import React from 'react';
-import { Header, Input } from 'components';
-import { Grid, Image, Button } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-import * as cx from 'classnames';
-import parkitLogin from 'images/parkitlogo.png';
-import styles from './styles.module.css'
+import React from "react";
+import { Header, Input } from "components";
+import { Grid, Image, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import * as cx from "classnames";
+import parkitLogin from "images/parkitlogo.png";
+import styles from "./styles.module.css";
 
 export default class AddVehicle extends React.Component {
+  rememberMe = () => {};
 
-    render() {
-        return (
-            <div>
-                <Header />
-                <Grid columns={3} divided stackable verticalAlign='middle'>
-                    <Grid.Row centered>
-                        <Grid.Column width={4}>
-                            {/*Image*/}
-                            <Image src={parkitLogin} className={cx(styles.fit, "center")} />
-                        </Grid.Column>
-                        <Grid.Column width={5}>
-                            {/*Login*/}
-                            <h1>Welcome!</h1>
-                            <div className="center">
-                                <Input type='text' placeholder="Username" className={styles.input} />
-                                <Input type='password' placeholder="Password" className={styles.input} />
-                                {/*TODO: Add forgotten password label */}
-                            </div>
-                            <Link to="home"><Button color="blue" className={styles.loginButton}>Login</Button></Link>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
-                            {/*Register*/}
-                            <h1>Still don't have an account?</h1>
-                            <div className="center">
-                                <Input type='text' placeholder="Username" className={styles.input} />
-                                <Input type='password' placeholder="Password" className={styles.input} />
-                                <Input type='password' placeholder="Confirm password" className={styles.input} />
-                                <Input type='text' placeholder="Name" className={styles.input} />
-                                <Input type='text' placeholder="Phone number" className={styles.input} />
-
-                            </div>
-                            <Link to='home'><Button color="blue" className={styles.loginButton}>Register</Button></Link>
-                            {/*TODO: Add some kind of regex validation here? */}
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Grid columns={3} divided stackable verticalAlign="middle">
+          <Grid.Row centered>
+            <Grid.Column width={4}>
+              {/*Image*/}
+              <Image src={parkitLogin} className={cx(styles.fit, "center")} />
+            </Grid.Column>
+            <Grid.Column width={5}>
+              {/*Login*/}
+              <h1>Welcome!</h1>
+              <form className="center">
+                <Input
+                  type="text"
+                  placeholder="Username"
+                  className={styles.input}
+                />
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  className={styles.input}
+                />
+                <div className={cx(styles.input, styles.checkbox)}>
+                  <input type="checkbox" id="rememberMe" />
+                  <label for="rememberMe">Remember me</label>
+                </div>
+              </form>
+              <Link to="home">
+                <Button color="blue" className={styles.loginButton}>
+                  Login
+                </Button>
+              </Link>
+              <div className="center">
+                <a href="#">Forgot your password? Click here.</a>
+              </div>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              {/*Register*/}
+              <h1>Still don't have an account?</h1>
+              <form className="center">
+                <Input
+                  type="text"
+                  placeholder="Username"
+                  className={styles.input}
+                />
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  className={styles.input}
+                />
+                <Input
+                  type="password"
+                  placeholder="Confirm password"
+                  className={styles.input}
+                />
+                <Input
+                  type="text"
+                  placeholder="Name"
+                  className={styles.input}
+                />
+                <Input
+                  type="text"
+                  placeholder="Phone number"
+                  className={styles.input}
+                />
+              </form>
+              <Link to="home">
+                <Button color="blue" className={styles.loginButton}>
+                  Register
+                </Button>
+              </Link>
+              {/*TODO: Add some kind of regex validation here? */}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
 }
