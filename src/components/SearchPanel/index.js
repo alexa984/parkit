1cbb42476d3f0b3c489591dba1252c3acfdbb07e
alexa984 <a.yovkova@hacksoft.io>
2020-05-29 20:56:38 +0300

    Show pay for place only when place is selected

--- src/components/SearchPanel/index.js
+++ src/components/SearchPanel/index.js
@@ -1,51 +1,111 @@
-
-import React from 'react';
-import { Icon, Search } from 'semantic-ui-react';
-import { AddFilter } from 'components';
-import styles from './styles.module.css';
-
+import React from "react";
+import { Icon, Search, Button } from "semantic-ui-react";
+import { AddFilter } from "components";
+import styles from "./styles.module.css";
 
 class SearchPanel extends React.Component {
   render() {
     return (
       <div className={styles.searchPanel}>
-        <Search icon={<Icon name="search" color="blue" />} placeholder="Search..." size="large" className={styles.searchInput} color="grey" />
+        <Search
+          icon={<Icon name="search" color="blue" />}
+          placeholder="Search..."
+          size="large"
+          className={styles.searchInput}
+          color="grey"
+        />
         <div className={styles.searchButtons}>
-          <button className={`${styles.dotsButton} ${styles.fc1}`}>See all spots</button>
-          <button className={`${styles.parkingButton} ${styles.fc1}`}>3 min.</button>
-          <button className={`${styles.parkingButton} ${styles.fc1}`}>5 min.</button>
-          <button className={`${styles.parkingButton} ${styles.fc1}`}>10 min.</button>
+          <button className={`${styles.dotsButton} ${styles.fc1}`}>
+            See all spots
+          </button>
+          <button className={`${styles.parkingButton} ${styles.fc1}`}>
+            3 min.
+          </button>
+          <button className={`${styles.parkingButton} ${styles.fc1}`}>
+            5 min.
+          </button>
+          <button className={`${styles.parkingButton} ${styles.fc1}`}>
+            10 min.
+          </button>
         </div>
         <div className={styles.searchButtons}>
-          <button className={`${styles.parkingButton} ${styles.fc1}`} onClick={this.props.selectHome}>Home</button>
-          <button className={`${styles.parkingButton} ${styles.fc1}`} onClick={this.props.selectUni}>Univesity</button>
-          <button className={`${styles.parkingButton} ${styles.fc1}`} onClick={this.props.selectOffice}>Office</button>
-          <button className={`${styles.parkingButton} ${styles.fc1}`}>Fitness</button>
+          <button
+            className={`${styles.parkingButton} ${styles.fc1}`}
+            onClick={this.props.selectHome}
+          >
+            Home
+          </button>
+          <button
+            className={`${styles.parkingButton} ${styles.fc1}`}
+            onClick={this.props.selectUni}
+          >
+            Univesity
+          </button>
+          <button
+            className={`${styles.parkingButton} ${styles.fc1}`}
+            onClick={this.props.selectOffice}
+          >
+            Office
+          </button>
+          <button className={`${styles.parkingButton} ${styles.fc1}`}>
+            Fitness
+          </button>
         </div>
 
         <AddFilter></AddFilter>
 
         <div className={styles.placesCards}>
-          <button className={styles.placeCard}>Restaurant<p>430places</p></button>
-          <button className={styles.placeCard}>Coffee<p>380places</p></button>
-          <button className={styles.placeCard}>Cinema<p>15places</p></button>
+          <button className={styles.placeCard}>
+            Restaurant<p>430places</p>
+          </button>
+          <button className={styles.placeCard}>
+            Coffee<p>380places</p>
+          </button>
+          <button className={styles.placeCard}>
+            Cinema<p>15places</p>
+          </button>
         </div>
 
         <div className={styles.favourites}>
           <h5>Favourites</h5>
           <div className={styles.favouriteButtons}>
-            <button className={styles.faveButton} onClick={this.props.selectHome}>Home<div><p>g.k. Ivan Vazov</p><p>7.3 km</p></div></button>
-            <button className={styles.faveButton} onClick={this.props.selectUni}>University<div><p>bul. James Boucher 5</p><p>0 km</p></div></button>
-            <button className={styles.faveButton} onClick={this.props.selectOffice}>Office<div><p>bul. Vitosha 87</p><p>2.4 km</p></div></button>
+            <button
+              className={styles.faveButton}
+              onClick={this.props.selectHome}
+            >
+              Home
+              <div>
+                <p>g.k. Ivan Vazov</p>
+                <p>7.3 km</p>
+              </div>
+            </button>
+            <button
+              className={styles.faveButton}
+              onClick={this.props.selectUni}
+            >
+              University
+              <div>
+                <p>bul. James Boucher 5</p>
+                <p>0 km</p>
+              </div>
+            </button>
+            <button
+              className={styles.faveButton}
+              onClick={this.props.selectOffice}
+            >
+              Office
+              <div>
+                <p>bul. Vitosha 87</p>
+                <p>2.4 km</p>
+              </div>
+            </button>
           </div>
         </div>
 
-
-        {/* TODO */}
-        <button className={styles.pay}>Pay for place</button>
-
+        {this.props.placeSelected && (
+          <Button className={styles.pay}>Pay for your parking place</Button>
+        )}
       </div>
-
     );
   }
 }
--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,51 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import { Header } from 'components';
-import styles from './styles.module.css';
-import SearchPanel from 'components/SearchPanel';
-import MapContainer from 'components/MapContainer';
+import { Header } from "components";
+import styles from "./styles.module.css";
+import SearchPanel from "components/SearchPanel";
+import MapContainer from "components/MapContainer";
 
 export default class Map extends React.Component {
   state = {
     homeSelected: false,
     uniSelected: false,
-    officeSelected: false
-  }
+    officeSelected: false,
+  };
 
   selectUni = () => {
     this.setState({
       homeSelected: false,
       uniSelected: true,
-      officeSelected: false
+      officeSelected: false,
     });
-  }
+  };
   selectHome = () => {
     this.setState({
       homeSelected: true,
       uniSelected: false,
-      officeSelected: false
+      officeSelected: false,
     });
-  }
+  };
   selectOffice = () => {
     this.setState({
       homeSelected: false,
       uniSelected: false,
-      officeSelected: true
+      officeSelected: true,
     });
-  }
+  };
 
   render() {
     const { homeSelected, uniSelected, officeSelected } = this.state;
+    const anySelected = homeSelected || uniSelected || officeSelected;
     return (
       <>
         <Header />
         <div className={styles.flexContainer}>
-          <MapContainer homeSelected={homeSelected} uniSelected={uniSelected} officeSelected={officeSelected} />
+          <MapContainer
+            homeSelected={homeSelected}
+            uniSelected={uniSelected}
+            officeSelected={officeSelected}
+          />
           <div className={styles.manageMap}>
             <SearchPanel
               selectUni={this.selectUni}
               selectHome={this.selectHome}
               selectOffice={this.selectOffice}
+              placeSelected={anySelected}
             />
           </div>
         </div>
