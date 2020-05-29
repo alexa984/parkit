7bf3d5eacd932f71fd914b892b1af5bbb038d96a
alexa984 <a.yovkova@hacksoft.io>
2020-05-30 00:01:19 +0300

    Fix logo position according to screen width

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,40 @@
+import React from "react";
 
-import React from 'react';
-
-import logo from '../../images/logo.png';
-import { Navbar, Nav } from 'react-bootstrap';
-import styles from './styles.module.css'
+import logo from "../../images/logo.png";
+import { Navbar, Nav } from "react-bootstrap";
+import styles from "./styles.module.css";
 
 class Header extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className={styles.navigationHeader}>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+        className={styles.navigationHeader}
+      >
         <Nav className="mr-auto">
-          <img src={logo} alt="logo" className={styles.logo} />
-          <Navbar.Brand href="/home" className={styles.navTitle}>ParkIt</Navbar.Brand>
+          <Navbar.Brand href="/home" className={styles.navTitle}>
+            <img src={logo} alt="logo" className={styles.logo} />
+            ParkIt
+          </Navbar.Brand>
         </Nav>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" className={styles.navLinkGroup}>
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className={styles.navLinkGroup}
+        >
           <Nav className="ml-auto">
-            <Nav.Link href="/home" className={styles.navLink}>Home</Nav.Link>
-            <Nav.Link href="/Map" className={styles.navLink}>Map</Nav.Link>
-            <Nav.Link href="/profile/index.js" className={styles.navLink}>Profile</Nav.Link>
+            <Nav.Link href="/home" className={styles.navLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="/Map" className={styles.navLink}>
+              Map
+            </Nav.Link>
+            <Nav.Link href="/profile/index.js" className={styles.navLink}>
+              Profile
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
